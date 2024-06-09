b1e775bfa51a665f90d25f9dc549bc121540a072
Samuel <samuelsiuyanyat@gmail.com>
2024-06-10 00:23:22 +0800

    axios linked

--- src/login.js
+++ src/login.js
@@ -1,26 +1,29 @@
 import React from 'react';
 
 
-function LoginPage() {
+function LoginPage () {
+  
 
-  return(
-    <>
-    <h1>Login Page</h1>
-    <form action=''>
-      <div className='input-group'>
-        <label>Username:
-          <input type='text' name='username'></input>
-        </label>
-        <br></br>
-        <label>Password:
-          <input type='password' name='password'></input>
-        </label>
-        <br></br>
-        <input type='submit' value={"Submit"}></input>
-      </div>
-    </form>
-    </>
-  );
+  
+    return(
+      <>
+      <h1>Login Page</h1>
+      <form action='http://localhost:8030/login' method='post'>
+        <div className='input-group'>
+          <label>Username:
+            <input type='text' name='username'></input>
+          </label>
+          <br></br>
+          <label>Password:
+            <input type='password' name='password'></input>
+          </label>
+          <br></br>
+          <input type='submit' value={"Submit"}></input>
+        </div>
+      </form>
+      </>
+    );
+  
 }
 
 export default LoginPage;
--- src/mathapp.js
+++ src/mathapp.js
@@ -1,23 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 
 function Mathapp() {
+
+    async function sendData() {
+        await axios.get('http://localhost:8030/dataOperation', {withCredentials: true})
+        .then(function(res) {
+            console.log(res)
+        }).catch((err) => {
+            console.log(err)
+        })
+    };
 
     
     return (
 
         <div className="App">
-                <div id='test'></div>
-                {/* <i className='fa fa-bars'></i> */}
-                <div className='turn-abacus'>
-                <h1>
-                    inline / abacus mode
-                    </h1>
-                    <div className='turning-location'>
-                    {/* <div className='turning-btn' onClick={abacusMode}></div> */}
-                    </div>
-                </div>
-
+            <div id='test'>
+            <button onClick={sendData}>
+                submit
+            </button>
             </div>
+            {/* <i className='fa fa-bars'></i> */}
+            <div className='turn-abacus'>
+            <h1>
+                inline / abacus mode
+                </h1>
+                <div className='turning-location'>
+                {/* <div className='turning-btn' onClick={abacusMode}></div> */}
+                
+                </div>
+            </div>
+
+        </div>
     )}
 
 
